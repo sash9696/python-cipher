f0c7f83071661633785c133fdb1f0c9ba3fa8700
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-10-01 10:40:30 +0530

    Add files via upload

---
+++ python/variables.py
@@ -0,0 +1,111 @@
+
+
+#Acceptable variable names
+
+# myname = 'Peter'
+# my_name = 'John'
+# _my_name = 'John'
+# myName = 'John'
+# MYNAME = 'john'
+# myname3 = 'john'
+
+#Not Acceptable
+# 2myname = 'John'
+# my-name = 'John'
+#  my name = 'John'
+
+
+myfirstname = 'John'
+
+#camel Case
+myFirstName = 'John'
+
+#Pascal Case
+MyFirstName = 'John'
+
+#Snake Case
+my_first_name = 'John'
+
+# print(my name)
+
+
+
+#assign multiple values to multiple variables
+# x = 10
+# y = 20
+# z = 30
+
+# x, y, z = 10, 20, 30 
+# print(x)
+# print(y)
+# print(z)
+
+#assign same values to multiple variables
+
+# x = y = z = 10
+
+# print(x,y,z)
+
+
+
+
+#output variables
+
+# x = 'My name is John'
+# print(x)
+
+# x = 'My '
+# y = 'name '
+# z = 'is'
+# # print(x,y,z)
+# print(x + y + z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = 'hello'
+# y = 10
+# print(x,y)
+# print(x + y)
+
+# x="My"
+# y='name'
+# z='is Ak'
+# print(x+' ' +y+' '+ z)
+
+
+#Global Variables
+# variables that are created out side of a function
+#they can be used outside and inside of the functions
+
+
+# x = 'John' #garden 2 or global variable
+
+# def myfunc(): #campus
+#     print("My name is " + x)
+    
+    
+# print(x)
+
+# myfunc()
+
+
+# x = 'John' #garden 2 or global variable
+
+# def myfunc(): #campus
+#     x = 'peter' #local variable
+#     print("My name is " + x)
+    
+# myfunc()
+# print(x)
+
+x = 'John' #garden 2 or global variable
+
+# def myfunc(): #campus
+#     y = 'peter' #local variable
+#     print("My name is " + x)
+    
+# myfunc()
+# print("My name is " + y)
+
