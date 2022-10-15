e81645ed118c0e4bac45879ae080511bbc6a1c3d
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-10-15 13:20:14 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,75 @@
+# Python Operators
+
+
+# Arithmetic Operators
+
+# print(10+20) #addition
+
+# print(20-10) #substraction
+
+# print(10*20) #multiplication
+
+# print(20/10) #division
+
+# print(20%10) #Modulus
+
+# print(15 // 2) #floor division  7.5 but ith rounds the result to the nearest whole number
+
+print(15 // 4)
+
+
+
+x = 2 
+y = 5
+
+
+print(x ** y) #//exponential same as 2*2*2
+
+
+
+# Assignment Operators
+
+a = 20
+
+# a += 5
+# a -= 5
+# a *= 5
+# a /= 5
+
+# a %= 5 # same as a = a % 5
+
+# a //=3 # a = a // 3
+
+a **= 2 # a = a**2 i.e 20*20
+
+
+print(a)
+
+
+# Comparison
+
+print(3 == 4) #Equal
+
+print(3 != 4) #Not equal
+
+print (3>4) #greater than
+
+print (3<4)  #less than
+
+print (3>=4) #greater than equal
+
+print (3<=4)  #less than equal
+
+# Logical Operators
+
+x = 2
+
+# print(x < 5 and x < 8) #Return True if both the statements are true
+# print(x < 5 and x > 8)
+
+# print(x < 5 or x > 8) #Return True, if anyone of the condition is met
+
+# print(x > 3 or x > 10)
+
+
+print(not(x < 5 and x < 8)) #reverses the result for exa -> if the result is true it will retuen false and vice versa
