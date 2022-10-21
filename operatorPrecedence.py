147757ed8a649f8dc097ee9aaf58565c1dd2f908
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-10-21 09:11:40 +0530

    Add files via upload

---
+++ operatorPrecedence.py
@@ -0,0 +1,77 @@
+# Operators precedence
+
+# mutliplication has a higher precedence over substraction
+# print(10 - 4 * 2)
+
+# paranthesis () has higher precedence over multiplication
+# print((10 - 4) * 2)
+
+# Associativity (Left-to-right in python)
+# when two operators have same precedence, then associativity helps us to determine the order of operations. Almost all the operators have left to right associativity.
+
+
+# print(5 * 2 // 3 )
+
+# print(5 // 2 * 3 )
+
+# Exponent operator has right to left associativity
+# print(  2 ** 3 ** 1) => (2 ** 3)
+
+
+#  x == y
+#  x != y
+#  x < y
+#  x > y
+#  x <= y
+#  x >= y
+
+# widely used in if-else conditions and loops
+
+# we start a if condition by writing if keyword
+
+
+# x = 20
+# y = 30
+
+# if x > y:
+#     print('y is greater than x')
+
+
+# collegeTime = 10
+
+# personEnteringCollege = 15
+
+# if personEnteringCollege < collegeTime:
+#     print('Go to college')
+# else :
+#     print('Go back to hostel')
+
+
+# if (2 > 5):
+#     print('hello')
+# else:
+#     print('bye')
+ 
+ 
+# if 10 > 5: print('Ten is greater than five') #One liner if condition
+
+# print('hello') if 5 > 5 else print('bye') #one liner for if-else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
