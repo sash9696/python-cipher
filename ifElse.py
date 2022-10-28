a4485211b938c2916e49b02ff0a9c5f5dcf9c906
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-10-28 09:02:13 +0530

    Add files via upload

---
+++ ifElse.py
@@ -0,0 +1,46 @@
+# x = 60
+# y = 60
+# z = 80
+
+# if(x<y): #checks for if if condition is true
+#     print('true')
+# elif x==y: #if if condition is not true then it checks for elif condition
+#     print('both numbers are same')
+# else: #it will take all of the rest conditions except the preceding ones
+#     print('false')
+
+
+x = 10
+
+y = 200
+
+z = 100
+
+# if(x<z and y<z):
+#     print('Both the conditions are true')
+
+# if(x<z or y<z):
+#     print('at least one conditon is met')
+
+
+# Nested if else conditions
+
+x = 55
+
+if(x < 60):
+   print('x is  less than 60')
+   if(x < 50):
+       print('x is also less than 50')
+   else:
+       print('x is either equal or greater than 50 to less than 60')
+else:
+    print('x is either equal or greater than 60')
+
+
+
+x = 60
+
+if(x < 40):
+    pass
+else:
+    print('x is less than 40')
