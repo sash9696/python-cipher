c3440e2571c62408a1f7404e5388fe6d8132a3ec
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-11-25 08:58:30 +0530

    Add files via upload

---
+++ format.cpp
@@ -0,0 +1,37 @@
+
+#age = 20
+
+#print('My name is John. My age is' + age)
+
+#format()
+
+#age = 20
+
+#text = 'My name is John. My age is {}'
+
+#print(text.format(age))
+
+#quantity = 5
+items = 3 money = 100
+
+    text = 'The amount for these {2} quantity is {1} rupees. Total items {0}'
+
+    print(text.format(items, money, quantity))
+
+#Escape Characters
+
+        print("We are \"good\" players of football")
+
+            print("Hello\nWorld")
+
+                text = 'my name is john'
+
+    print(text.capitalize())
+
+        print('HELLO'.casefold())
+
+            print('My age is 20. My lucky num is also 20'.count('is'))
+
+                print('My age is 20. My lucky num is also 20'.endswith('.'))
+
+                    print('My age is 20. My lucky num is also 20'.find('z'))
